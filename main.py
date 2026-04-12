de1c41c4bed6f954780daa397bfc3885cc89682e
Long Bui <63569684+longbv11@users.noreply.github.com>
2026-04-12 18:20:43 +0200

    amended docstrings and get_modes func

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ trips = pd.read_csv(f'{datapath}/trips.txt', dtype={'trip_id': str, 'route_id': 
 stops = pd.read_csv(f'{datapath}/stops.txt', dtype={'stop_id': str}).set_index('stop_id')
 stop_times = pd.read_csv(f'{datapath}/stop_times.txt', dtype={'stop_id': str, 'trip_id': str})
 
-#strip diacritics and create a new stop_name_normal column (normalized: no diacritics, all lowercase)
+#strip diacritics and create a new stop_name_normal column (normalized means no diacritics, all lowercase)
 import unicodedata
 def strip_accents(text):
     """Strips accents from a string"""
@@ -18,11 +18,11 @@ trips['trip_headsign_normal'] = trips['trip_headsign'].apply(strip_accents).str.
 
 def get_modes(stop_name):
     '''
-    Returns the modes that serve a stop, for a given normalized name
+    Returns the modes that serve a stop, for a given NORMALIZED name
+    e.g. get_modes('harminckettesek tere')
     '''
     matched_stop_id = stops[stops['stop_name_normal'] == stop_name].index
     matched_trip_id = stop_times[stop_times['stop_id'].isin(matched_stop_id)]['trip_id'].unique()
     matched_route_id = trips.loc[matched_trip_id, 'route_id'].unique()
     
     return routes.loc[routes.index.isin(matched_route_id), ['route_short_name', 'route_type', 'route_desc']]
-    
