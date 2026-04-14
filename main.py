0ebb4f48c58509bf9e9288b46d3031ef480cf80e
Long Bui <63569684+longbv11@users.noreply.github.com>
2026-04-14 14:56:52 +0200

    improved readability for get_modes and new get_timetable func

--- main.py
+++ main.py
@@ -24,5 +24,48 @@ def get_modes(stop_name):
     matched_stop_id = stops[stops['stop_name_normal'] == stop_name].index
     matched_trip_id = stop_times[stop_times['stop_id'].isin(matched_stop_id)]['trip_id'].unique()
     matched_route_id = trips.loc[matched_trip_id, 'route_id'].unique()
-    
-    return routes.loc[routes.index.isin(matched_route_id), ['route_short_name', 'route_type', 'route_desc']]
+
+    resultModes = routes.loc[routes.index.isin(matched_route_id), ['route_short_name', 'route_type', 'route_desc']]
+    modeMap = {0: 'tram', 3: 'bus', 11: 'trolleybus'}
+    resultModes['route_type'] = resultModes['route_type'].replace(modeMap)
+
+    return resultModes.rename(columns={'route_short_name': 'Route Number', 'route_type': 'Mode', 'route_desc': 'Destination'})
+
+
+def get_timetable(stop_name, route_filter = None, hour_filter = None):
+    '''
+    Returns a schedule of departure times and routes, for a given normalized stop name.
+    e.g. get_timetable('harminckettesek tere', 9, 8)
+    '''
+    matched_stop_id = stops[stops['stop_name_normal'] == stop_name].index
+    ttable = stop_times[stop_times['stop_id'].isin(matched_stop_id)][['trip_id', 'departure_time']]
+    ttable = ttable.sort_values('departure_time')
+    ttable = ttable.merge(trips[['trip_headsign', 'route_id']], left_on='trip_id', right_index = True)
+    ttable = ttable.merge(routes[['route_short_name']], left_on='route_id', right_index = True)
+
+    if route_filter is not None:
+        ttable = ttable[ttable['route_short_name'] == str(route_filter)]
+        
+    if hour_filter is not None:
+        hour_str = str(hour_filter).zfill(2)
+        ttable = ttable[ttable['departure_time'].str.startswith(f"{hour_str}:")]
+        
+    return ttable[['departure_time', 'route_short_name', 'trip_headsign']].rename(columns = {
+        'departure_time': 'Time', 
+        'route_short_name': 'Route No', 
+        'trip_headsign': 'Destination'
+        })
+
+# TODO: all the funcs below  
+
+def get_route_stops(route_short_name, direction_id=0):
+    '''
+    Returns an ordered list of stops for a given route, sorted by stop_sequence.
+    '''
+    pass
+
+def get_direct_connection(start_stop, end_stop):
+    '''
+    Returns a list of routes that provide a direct connection between two normalized stop names.
+    '''
+    pass
