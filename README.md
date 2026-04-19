7f64cb4651fe27b0555cc12a707eac39a7dbe6d7
Long Bui <63569684+longbv11@users.noreply.github.com>
2026-04-19 22:51:23 +0200

    new get_direct_connection pathfinding feature, hour filter for get_modes, enhanced README

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # BKKData, an entry-level data science personal project
 
+### Introduction
+
 This is a CLI program written using Python and Pandas in order to filter through and wrangle GTFS data. The program is intended to be run as a Jupyter notebook, but I prefer writing in python files and using VSCode's interactive window function to run code blocks. 
 
 Theoretically the program should work with any properly formatted GTFS-Static file. However, this specific program is intended to handle specifically [Budapest's data acquired from BKK OpenData](https://opendata.bkk.hu/home), hence the name.
 
 This is a work in progress.
+
+### Current functionality:
+- `get_modes(stop_name, hour_filter=None)`: Returns the modes that serve a stop, for a given normalized name.
+- `get_timetable(stop_name, route_filter=None, hour_filter=None)`: Returns a schedule of departure times and routes, for a given normalized stop name.
+- `get_route_stops(route_number, direction_id=0)`: Returns an ordered list of stops for a given route, sorted by stop_sequence.
+- `get_direct_connection(start_stop, end_stop, hour_filter=None)`: Returns a list of routes that provide a direct connection between two normalized stop names.
--- main.py
+++ main.py
@@ -16,13 +16,19 @@ stops['stop_name_normal'] = stops['stop_name'].apply(strip_accents).str.lower()
 routes['route_name_normal'] = routes['route_desc'].apply(strip_accents).str.lower()
 trips['trip_headsign_normal'] = trips['trip_headsign'].apply(strip_accents).str.lower()
 
-def get_modes(stop_name):
+def get_modes(stop_name, hour_filter=None):
     '''
     Returns the modes that serve a stop, for a given NORMALIZED name
-    e.g. get_modes('harminckettesek tere')
+    e.g. get_modes('harminckettesek tere', hour_filter=14)
     '''
     matched_stop_id = stops[stops['stop_name_normal'] == stop_name].index
-    matched_trip_id = stop_times[stop_times['stop_id'].isin(matched_stop_id)]['trip_id'].unique()
+    
+    st = stop_times[stop_times['stop_id'].isin(matched_stop_id)]
+    if hour_filter is not None:
+        hour_str = str(hour_filter).zfill(2)
+        st = st[st['departure_time'].str.startswith(f"{hour_str}:")]
+        
+    matched_trip_id = st['trip_id'].unique()
     matched_route_id = trips.loc[matched_trip_id, 'route_id'].unique()
 
     resultModes = routes.loc[routes.index.isin(matched_route_id), ['route_short_name', 'route_type', 'route_desc']]
@@ -59,6 +65,7 @@ def get_timetable(stop_name, route_filter = None, hour_filter = None):
 def get_route_stops(route_number, direction_id=0):
     '''
     Returns an ordered list of stops for a given route, sorted by stop_sequence.
+    e.g. get_route_stops(83, 0)
     '''
     matched_route_id = routes[routes['route_short_name'] == str(route_number)].index
     matched_trips = trips[(trips['route_id'].isin(matched_route_id)) & (trips['direction_id'] == direction_id)]
@@ -71,9 +78,70 @@ def get_route_stops(route_number, direction_id=0):
 
     return (f'Stops for route number {route_number} on direction {direction_id}:'), route_stops[['stop_sequence', 'stop_name']].set_index('stop_sequence')
 
-# TODO: all the funcs below 
-def get_direct_connection(start_stop, end_stop):
+def get_direct_connection(start_stop, end_stop, hour_filter = None):
     '''
     Returns a list of routes that provide a direct connection between two normalized stop names.
+    e.g. get_direct_connection('astoria', 'kalvin ter', 14)
+    '''
+    start_stopid = stops[stops['stop_name_normal'] == start_stop].index
+    end_stopid = stops[stops['stop_name_normal'] == end_stop].index
+    
+    st_start = stop_times[stop_times['stop_id'].isin(start_stopid)][['trip_id', 'stop_sequence', 'departure_time']]
+    if hour_filter is not None:
+        hour_str = str(hour_filter).zfill(2)
+        st_start = st_start[st_start['departure_time'].str.startswith(f"{hour_str}:")]
+        
+    st_start = st_start[['trip_id', 'stop_sequence']]
+    st_end = stop_times[stop_times['stop_id'].isin(end_stopid)][['trip_id', 'stop_sequence']]
+
+    common_trip = st_start.merge(st_end, on = 'trip_id', suffixes = ('_start', '_end'))
+    valid_trip = common_trip[common_trip['stop_sequence_start'] < common_trip['stop_sequence_end']]
+
+    if valid_trip.empty:
+        return (f'No direct connection between {start_stop} and {end_stop}, please contact BKK to request the creation of such a route')
+    
+    valid_trips_info = trips.loc[valid_trip['trip_id'].unique(), ['route_id', 'trip_headsign']].drop_duplicates()
+    result = valid_trips_info.merge(routes[['route_short_name', 'route_type']], left_on='route_id', right_index=True)
+    
+    modeMap = {0: 'tram', 3: 'bus', 11: 'trolleybus', '0': 'tram', '3': 'bus', '11': 'trolleybus'}
+    result['route_type'] = result['route_type'].replace(modeMap)
+    
+    return result[['route_short_name', 'route_type', 'trip_headsign']].rename(columns={
+        'route_short_name': 'Route Number', 
+        'route_type': 'Mode', 
+        'trip_headsign': 'Destination'
+    }).reset_index(drop=True)
+
+#TODO: all these by tuesday
+
+def get_route_frequency(route_number, stop_name, start_hour=7, end_hour=9):
+    '''
+    Calculates the average headway (time between departures) in minutes for a specific route 
+    at a given stop during a specified time window (e.g., morning peak hours).
     '''
     pass
+
+def get_busiest_stops(top_n=10):
+    '''
+    Analyzes the entire network to find the stops with the highest volume of daily departures 
+    or the highest number of unique intersecting routes.
+    '''
+    pass
+
+def get_transfer_connection(start_stop, end_stop):
+    '''
+    An upgrade to get_direct_connection that finds a path between two stops requiring exactly 
+    ONE transfer. Returns the suggested transfer station and both route numbers.
+    '''
+    pass
+
+def get_service_span(route_number, direction_id=0):
+    '''
+    Returns the first departure (opening) and last departure (closing) times for a route, 
+    calculating the total active hours of service for the day.
+    '''
+    pass
+
+
+
+    
